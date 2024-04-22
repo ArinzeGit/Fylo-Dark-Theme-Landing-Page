adf4a561cb89c41aef2885d5682322fd3fe2d996
Arinze Owoh <arinzeowoh@gmail.com>
2024-04-22 21:20:34 +0100

    edit image path to fix image not displaying

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ const Logo = () => {
   return (
     <img
       style={{ width: "12.1vw", height: "auto" }}
-      src="./src/assets/images/logo.svg"
+      src="src/assets/images/logo.svg"
       alt="Fylo Logo"
     />
   );
